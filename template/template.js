4147238dc71150057e4c64a961a334e4ef79be69
Vijay Ravi Adyanthaya <vadyangthaya@yahoo.co.uk>
2020-02-02 13:52:08 +0000

    template pattern case completed

--- template/template.js
+++ template/template.js
@@ -11,3 +11,39 @@ class ColorTemplate extends PaperTemplate {
     document.write("Set the Color of A4 paper red <br>");
   }
 }
+// space attack classes
+class Sprite {
+  constructor() {
+    this.x=0 //Airplane x coordinates.
+    this.y=0 //airplane y coordinates.
+    this.image=null;
+  }
+  draw(context){
+    context.drawImage(this.image,this.x,this.y);
+  }
+  getX(){
+    return this.x;
+  }
+  getY(){
+    return this.y;
+  }
+  setX(x){
+    this.x=x;
+  }
+  setY(y){
+    this.y=y
+  }
+  getImage(){
+    return this.image;
+  }
+  setImage(image){
+    this.image=image;
+  }
+
+}
+class MyFlane extends Sprite{
+
+}
+class EnemyFlane extends Sprite{
+
+}
