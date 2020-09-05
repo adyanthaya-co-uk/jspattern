e2fd33ccecb3d6e3bc2e1542f1792380397900bd
Vijay Ravi Adyanthaya <vadyangthaya@yahoo.co.uk>
2020-09-05 04:18:13 +0100

    commit changes
    commit

--- builderpattern/builder.js
+++ builderpattern/builder.js
@@ -1,6 +1,63 @@
 // builder class
-class ClassName {
+class Car {
   constructor() {
+    this.head="";
+    this.body="";
+    this.wheel="";
 
+  }
+  getHead(){
+    return this.head;
+  }
+  setHead(head){
+    this.head=head;
+  }
+  getBody(){
+    return this.body;
+  }
+  setBody(body){
+    this.body=body;
+  }
+  getWheel(){
+    return this.wheel;
+}
+setWheel(wheel){
+  this.wheel=wheel;
+}
+}
+
+class Builder {
+  buildHead(){}
+  buildBody(){}
+  buildWheel(){}
+  buildPart(){}
+
+}
+class ConcreteBuilder extends Builder {
+  constructor() {
+    super();
+    this.car=new Car();
+
+  }
+  buildHead(){
+    this.car.setHead("Car Head Construction Completed !");
+  }
+  buildBody(){
+    this.car.setBody("Car Body Construction Completed !");
+  }
+  buildWheel(){
+      this.car.setWheel("Car Wheel construction Completed !");
+
+  }
+  buildPart(){
+    return this.car;
+  }
+}
+class Director{
+  static construct(builder){
+    builder.buildHead();
+    builder.buildBody();
+    builder.buildWheel();
+    return builder.buildPart();
   }
 }
